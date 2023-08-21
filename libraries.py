eae8ed9d6d87e997ab65769815a2f30638e58162
bb610 <78195053+bb610@users.noreply.github.com>
2023-08-22 00:05:24 +0100

    Create libraries.py

---
+++ libraries.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' #info and warning messages are not printed. Only error messages printed. Disables all logging output from TensorFlow.
+import time
+import matplotlib.pyplot as plt
+import cv2
+import seaborn as sns
+sns.set_style('darkgrid')
+import shutil
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Dense, Activation,Dropout,Conv2D, MaxPooling2D,BatchNormalization
+from tensorflow.keras.optimizers import Adam, Adamax
+from tensorflow.keras.metrics import categorical_crossentropy
+from tensorflow.keras import regularizers
+from tensorflow.keras.models import Model
